e28f87f548e3ef8dc7ad70687ba456b2cfa13973
Daniel Amaral <silva.daniel86@Gmail.com>
2022-03-27 12:01:54 -0300

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -29,7 +29,7 @@ def model_net(pretrained = False, **kwargs):
   if pretrained:
     dirname = os.path.dirname(__file__)
     checkpoint = os.path.join(dirname, 'weights/save.pth')
-    state_dict = thload(checkpoint)
+    state_dict = tload(checkpoint)
     model.load_state_dict(state_dict)
 
   return model
