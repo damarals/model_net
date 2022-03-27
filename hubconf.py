65c77c84278df62033abf6efe250766eb725290d
Daniel Amaral <silva.daniel86@Gmail.com>
2022-03-27 11:55:57 -0300

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -18,7 +18,7 @@ def model_net(pretrained = False, **kwargs):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = flatten(x, 1) # flatten all dimensions except batch
+        x = flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
